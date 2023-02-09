e801baf9302cb81e6ab2c49d44f54889ab8b158b
mesfik <111147948+mesfik@users.noreply.github.com>
2023-02-09 23:12:25 +0300

    update base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import uuid
-import datetime
+from datetime import datetime
 """
 A python module for a base model of airbnb
 """
@@ -18,14 +18,16 @@ class BaseModel():
         Args: *args: non key worded argument
               **kwargs: key worded arguments
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
-
-        for keys, value in kwargs.items():
-            if key in ['id', 'created_at', 'updated_at']:
-                continue
-            setattr(self, key, value)
+        if kwargs:
+            for key, value in kwargs.items():
+                if (key != '__class__'):
+                    setattr(self, key, value)
+            self.created_at = datetime.strptime(self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
+            self.updated_at = datetime.strptime(self.updated_at, '%Y-%m-%dT%H:%M:%S.%f')
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -41,7 +43,7 @@ class BaseModel():
         """
         A public instance method that updates datetime
         """
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """
