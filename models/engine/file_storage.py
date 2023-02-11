cb0a18c40810dfc852df63938d92843998998b19
mesfik <111147948+mesfik@users.noreply.github.com>
2023-02-11 10:00:39 +0300

    file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,15 +10,12 @@ class FileStorage:
     """ A class that serializes instances to a JSON file
     and deserializes JSON file to instances
     """
-
-    __file_path = "file.json"
-
-    def __init__(self):
-        """
-        Initialization of private instance attribute
+    def __init__(self, file_path='file.json'):
+        """ Initialization a private instance attributes
         Args:
-             file_path: initialized file path to file.json
+             file_path :initialized file path to file.json
         """
+        self.__file_path = file_path
         self.__objects = {}
 
     def all(self):
@@ -33,27 +30,25 @@ class FileStorage:
         Args:
              obj: the object with a key <class name>.id
         """
-
-        key = obj.__class__.__name__
-        self.__objects["{}.{}".format(key, obj.id)] = obj
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
         """ A public instance method that serilizes object to json
         """
-        odict = self.__objects
-        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
-        with open(FileStorage.__file_path, 'w') as file:
-            json.dump(objdict, file)
+        objects = self.__objects
+        obj_dict = {obj_id: objects[obj_id].to_dict() for obj_id in objects.keys()}
+        with open(self.__file_path, 'w') as file:
+            json.dump(obj_dict, file)
 
     def reload(self):
         """ A public instance method to deserialize the json
         """
         try:
-            with open(FileStorage.__file_path, 'r') as file:
+            with open(self.__file_path, 'r') as file:
                 objdict = json.load(file)
                 for o in objdict.values():
                     cls_name = o["__class__"]
-                    del o["__class__"]
                     self.new(eval(cls_name)(**o))
         except Exception:
             pass
