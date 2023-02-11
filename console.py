511bb4885f809204f23be4b305755a7b5e067555
mesfik <111147948+mesfik@users.noreply.github.com>
2023-02-12 00:05:48 +0300

    console

---
+++ console.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+the console module
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """A class for the command interprator"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        """Quit command to exit the program
+
+        """
+        return True
+
+    def do_EOF(self, line):
+        """
+        EOF method used to exit the program
+        """
+        return True
+
+    def emptyline(self):
+        """
+        A method empty line to don't excute anything
+        """
+        return
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
